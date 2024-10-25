1cd0903e77121199d708443a2fb8783c0257157c
Alexandre Rademaker <arademaker@gmail.com>
2024-10-25 13:10:41 -0300

    xml handle

--- Main.lean
+++ Main.lean
@@ -1,4 +1,8 @@
 import Vizagrams
+import Lean.Data.Xml
+import Lean.Data.RBMap
+
+open Lean (RBMap)
 
 def test1 := (Primitive.circle.mk (12, 12) 4)
 def test2 := (Primitive.rectangle.mk (10, 10) 30 30)
@@ -6,6 +10,7 @@ def test3 := (Primitive.ellipse.mk (75, 75) 20 5)
 def test4 := (Primitive.line.mk (10, 110) (50, 150))
 def test5 := (Primitive.polyline.mk [(60, 110), (65, 120), (70, 115), (75, 130), (80, 125), (85, 140), (90, 135), (95, 150), (100, 145)]) -- Polyline
 def test6 := (Primitive.polygon.mk [(0,100), (50,25), (50,75), (100,0)])
+
 #eval SVG.Primitive.draw test1
 #eval SVG.Primitive.draw test2
 #eval SVG.Primitive.draw test3
@@ -13,3 +18,11 @@ def test6 := (Primitive.polygon.mk [(0,100), (50,25), (50,75), (100,0)])
 #eval SVG.Primitive.draw test5
 #eval SVG.Primitive.draw test6
 
+
+def test := Lean.Xml.Element.Element "tag"
+  (RBMap.fromList [("attr", "value")] compare)
+  #[Lean.Xml.Content.Character "alexandre"]
+
+#eval test
+
+#check Lean.Xml.parse "<svg><circle cx='12' cy='12' r='4' /></svg>"
