e3556ed084bab0efc5d2a1e74e534e9ec01dfb07
Jhonny <carvalhoborgeshenrique@gmail.com>
2025-01-23 09:33:33 -0300

    passando LeanViz

--- Main.lean
+++ Main.lean
@@ -1,25 +1,19 @@
 import Vizagrams
-import Lean.Data.Xml
-import Lean.Data.RBMap
 
-open Lean (RBMap)
-open Lean Xml
-open Primitive
+def Vizagrams := "Vizagrams"
 
--- definndo circles
-def c1 : circle Float := { center := (50.0, 50.0), radious := 5.0 }
-def c2 : circle Float := { center := (0.0, 0.0), radious := 15.0 }
+#eval Vizagrams
 
-def rect : rectangle Float := { origin := (1.0,1.0), width := 10.0 , height := 10.0 }
-#check PrimToSvg.primToSvg c1 -- Type Element
 
-def positionedCircle := placeCircleNextTo (c1) (c2) (1.0 , 1.0)
+open ProofWidgets Svg in
+private def frame : Frame where
+  xmin   := -2
+  ymin   := -2
+  xSize  := 4
+  width  := 400
+  height := 400
+private def x := @ProofWidgets.Svg.line frame (0.,0.) (1.,0.)
 
-#eval positionedCircle
-
-def svgElement : Lean.Xml.Element :=
-  Lean.Xml.Element.Element "svg"
-    (RBMap.fromList [("xmlns", "http://www.w3.org/2000/svg"), ("width", "200"), ("height", "100")] compare)
-    #[Content.Element (PrimToSvg.primToSvg c1), Content.Element (PrimToSvg.primToSvg rect)]
-
-#eval svgElement
+-- Desenhar circle
+def circle : Primitives.Circle := Primitives.Circle.mk 1 ⊞[0,0]
+#eval circle
