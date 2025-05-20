b0e82b5e08e5fcd57970d981364e30ba459a7a78
Jhonny <carvalhoborgeshenrique@gmail.com>
2025-05-20 20:04:55 -0300

    Exemple GraphicExpression and new Mark Nil

--- Test/Exemple.lean
+++ Test/Exemple.lean
@@ -7,31 +7,30 @@ open ProofWidgets Svg
 open GraphicalMark
 open FreeMonad
 
--- Example 1: Drawing a "BarChart"
+-- Example: Drawing a "BarChart"
 def data₁ : List Float :=  [1.0, 2.3, 0.7, 3.1, 0.3]
-
-def coalgbar (τ : List Float) : List ( List Float ) :=
-  τ.map (fun x => [x])
 
 def barPolygon (h : Float) (w : Float := 0.8) : Prim :=
   let pts : Array Vec2 :=
     #[![0, 0], ![w, 0], ![w, h], ![0, h]]
   NewPolygon pts {fillColor := Color.mk (3*h/2) (2*h/3) (h/5) }
 
+def coalgbar (τ : List Float) : List ( List Float ) :=
+  τ.map (fun x => [x])
+
 def barExpr : List Float → 𝕋 Mark
   | [h] => 𝕋.pure (barPolygon h)
-  | _   => 𝕋.pure (NewCircle 0 ![0,0])
+  | _   => 𝕋.pure (Nil.mk)
 
 def algBar (τ : List (𝕋 Mark)) : 𝕋 Mark :=
   match τ with
   | x :: xs => List.foldl (· →[0.5] ·) x xs
-  | [] => 𝕋.pure (NewCircle 0 ![0,0])
+  | [] => 𝕋.pure Nil.mk
 
 def bars : GraphicExpression (List Float):= {
   expr := barExpr
   coalg := coalgbar
   alg := algBar
 }
-#check bars.eval data₁
 
-#html Draw ( bars.eval data₁)
+#html draw ( bars.eval data₁)
