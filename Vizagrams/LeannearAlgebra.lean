093aa10050129a759aa34265781e9a69ebed7144
Jhonny <carvalhoborgeshenrique@gmail.com>
2025-08-30 15:49:12 -0300

    feat: Add norm notation and operators for matrix and vector products
    Used the composition operator ∘ for matrix multiplication and @ for matrix-vector multiplication.
    
    This choice is based on the fact that matrices represent linear transformations, so using the function composition operator is a natural fit. The choice of @ was inspired by vector libraries like NumPy.

--- Vizagrams/LeannearAlgebra.lean
+++ Vizagrams/LeannearAlgebra.lean
@@ -6,6 +6,10 @@ Authors: Henrique Borges
 import Mathlib.Analysis.InnerProductSpace.Basic
 import Mathlib.Data.Matrix.auto
 import Mathlib.Data.Matrix.Notation
+import Mathlib.Analysis.Normed.Group.Basic
+import Mathlib.LinearAlgebra.AffineSpace.AffineMap
+import Mathlib.Data.Matrix.Basic
+import Mathlib.Data.Matrix.Notation
 
 /-!
 # Linear Algebra Foundations for Vizagrams
@@ -14,22 +18,26 @@ library. It establishes the `Vec2` type for 2D vectors over `Float`, provides st
 instances for vector space operations, and defines core geometric transformations like rotation,
 scaling, and translation using `Mathlib`'s `LinearMap` and `AffineMap`.
 -/
-
 open Matrix Fin
 def π : Float := 3.141592653589793 -- Aproximação para π em Float
+
+/-# Fin
+Fin n representa o conjunto `Iₙ` ou `[n]`, isto é, os Naturais menores que n
+podemos usar `Fin n` para indexar vetores com n posições
+`Fin 2 → ℝ²` é o vetor de duas entradas reais
+-/
 abbrev Vec2 := Fin 2 → Float
 
 instance : Inner Float Vec2 where
   inner v₁ v₂ := v₁ 0 * v₂ 0 + v₁ 1 * v₂ 1
 
 notation "⟪" x "," y "⟫" => inner Float x y
-/-# Fin
-Fin n representa o conjunto `Iₙ` ou `[n]`, isto é, os Naturais menores que n
-podemos usar `Fin n` para indexar vetores com n posições
-`Fin 2 → ℝ²` é o vetor de duas entradas reais
--/
 
-abbrev Mat2 := Matrix (Fin 2) (Fin 2) Float
+def norm (v : Vec2) : Float :=
+  Float.sqrt (⟪v, v⟫)
+
+notation:max "‖" v:max "‖" => norm v
+
 /-# Mat2
 `Defs.lean:` (Mathlib.Data.Matrix)
 def Matrix (m : Type u) (n : Type u') (α : Type v) : Type max u u' v :=
@@ -39,21 +47,17 @@ uma matriz é uma função que, dado um índice de linha `m` e um de coluna `n`,
 retorna o elemento daquela posição
 -/
 -- Usar `!![ ; ]` vem de Matrix.Notation
+abbrev Mat2 := Matrix (Fin 2) (Fin 2) Float
 
+def matMul (A B : Mat2) : Mat2 :=
+  fun i j => A i 0 * B 0 j + A i 1 * B 1 j
 
+infixl:70 "∘" => matMul
 
--- # Definir Norma de vetor
-def Vec2Norm (v : Vec2) : Float :=
-  Float.sqrt (v 0 ^ 2 + v 1 ^ 2)
+def mulVec (A : Mat2) (v : Vec2) : Vec2 :=
+  fun i => (A i 0) * v 0 + (A i 1) * v 1
 
--- # Normalização de vetor
-def normalize (v : Vec2) : Vec2 :=
-  let n := Vec2Norm v
-  if n == 0 then v else fun i => v i / n
-
-class AffineMapLike (G : Type) (V : Type) where
-  eval : G → V → V
-  compose : G → G → G
+infixl: 65 "@" => mulVec
 
 -- Aqui definimos um Mat2Vec2, isto é, um tipo para representar x ↦ Ax + B
 structure Mat2Vec2 where
@@ -61,20 +65,13 @@ structure Mat2Vec2 where
   b : Vec2
 deriving Repr
 
--- # Produto de Matrizes e Vetores em Float²
+class AffineMapLike (G : Type) (V : Type) where
+  eval : G → V → V
+  compose : G → G → G
 
--- **Remover dotProd**
-def dotProd (v₁ v₂ : Vec2) : Float :=
-  v₁ 0 * v₂ 0 + v₁ 1 * v₂ 1
-
-def ScalarMul (s : Float) (v : Vec2) : Vec2 :=
-  fun i => s * v i
-
-def mulVec (A : Mat2) (v : Vec2) : Vec2 :=
-  fun i => (A i 0) * v 0 + (A i 1) * v 1
-
-def matMul (A B : Mat2) : Mat2 :=
-  fun i j => A i 0 * B 0 j + A i 1 * B 1 j
+instance : AffineMapLike Mat2Vec2 Vec2 where
+  eval f x := mulVec f.A x + f.b
+  compose f g := { A := f.A ∘ g.A , b := (f.A @ g.b) + f.b }
 
 def nullVec2 : Vec2 := ![0,0]
 
@@ -97,9 +94,6 @@ Agora seja g : V² → V² tal que g(x) := Cx + d, temos que
 (f ∘ g)(x) = f(g(x)) = f( Cx + d ) = A ( Cx + d ) + b = (A·C)x + (A · d + b)
 -/
 
-instance : AffineMapLike Mat2Vec2 Vec2 where
-  eval f x := mulVec f.A x + f.b
-  compose f g := { A := matMul f.A g.A , b := (mulVec f.A g.b) + f.b }
 
 infixr:70 " ⬝ " => AffineMapLike.eval (G := Mat2Vec2) (V := Vec2)
 infixr:70 " ∘ " => AffineMapLike.compose (G := Mat2Vec2) (V := Vec2)
@@ -121,7 +115,6 @@ def rotate (θ : Float) : Mat2Vec2 :=
 def getCoordinates (v : Vec2) : String :=
   s!"{v 0} {v 1}"
 
-/-
 -- Testes
 private def v : Vec2 := ![1,1]
 #eval v + v
@@ -130,4 +123,5 @@ private def v₁ : Vec2 := ![1.0, 2.0]
 private def v₂ : Vec2 := ![3.0, 4.0]
 #eval (inner Float v₁ v₂)
 #eval ⟪ v₁ , v₂ ⟫
--/
+#eval norm v
+#eval (‖ v ‖)
